f6ecd799b250eb3b8395e9f7432879daffd6ab2a
Eric Clack <eric@bn7.net>
2019-01-03 09:59:19 +0000

    Use piano to make example simpler

--- src/overtone_loops/music/largo.clj
+++ src/overtone_loops/music/largo.clj
@@ -1,74 +1,68 @@
 (ns overtone-loops.music.largo
-  (:use [overtone.live])
+  (:use [overtone.live]
+        [overtone.inst.piano])
   (:require [overtone-loops.loops :refer [defphrase metro
                                           on-next-bar]]))
 
-;; Define some instruments
-(definst tone 
-  "Sine tone that lasts about a second"
-  [freq 440 amp 0.7 sustain 0.8]
-  (let [env (env-gen (lin :sustain sustain :release 0.5) :action FREE)
-        src (sin-osc freq)]
-    (* amp env src)))
 
-;; (tone 440 :sustain 2)
-;; (tone 220 0.2)
-
-(defn t
-  "Tone player"
+(defn p
+  "Piano player that takes note symbols such as :c
+  Other ags:
+  gate vel decay release hard velhard muffle velmuff velcurve stereo tune random stretch sustain"
   [note-name & args]
-  (print (first args))
-  (let [amp (or (first args) 0.8)
-        sus (or (second args) 0.8)
-        freq (midi->hz (note note-name))]
-    (tone freq amp sus)))
+  (apply piano (note note-name) args))
 
-;; (t :c4)
-;; (t :d4 0.2)
+;; (p :c4 :vel 50)
+;; (p :d4 0.2)
 
 
 (metro-bpm metro 60)
 
 (defphrase part1
-  0 (t :b3 1)
-  1 (t :c4)
-  2 (t :d4)
+  0 (p :b3 :vel 100)
+  1 (p :c4 :vel 80)
+  2 (p :d4 :vel 80)
 
-  3 (t :g3 1)
-  4 (t :f#3)
-  5 (t :g3)
+  3 (p :g3 :vel 100)
+  4 (p :f#3)
+  5 (p :g3)
 
-  6 (t :c4 1)
-  7 (t :b3)
-  7.5 (t :g3)
-  8 (t :d4)
-  8.5 (t :b3)
+  6 (p :c4 :vel 100)
+  7 (p :b3)
+  7.5 (p :g3)
+  8 (p :d4)
+  8.5 (p :b3)
 
-  9 (t :b3 1)
-  10 (t :a3 0.8 2)
+  9 (p :b3 :vel 100)
+  10 (p :a3 :vel 80 :decay 1)
   ;;
   
-  12 (t :g3 1)
-  13 (t :g4)
-  14 (t :f#4)
-  14.5 (t :e4)
+  12 (p :g3 :vel 100)
+  13 (p :g4)
+  14 (p :f#4)
+  14.5 (p :e4)
 
-  15 (t :c#4 1)
-  16 (t :d4)
-  16.5 (t :a4)
-  17 (t :e4)
+  15 (p :c#4 :vel 100)
+  16 (p :d4)
+  16.5 (p :a4)
+  17 (p :e4)
 
-  18 (t :f#4 1)
-  18.4 (t :d4)
-  19 (t :e4 0.8 2)
+  18 (p :f#4 :vel 100)
+  18.5 (p :d4 :vel 80)
+  19 (p :e4 :vel 80 :decay 1)
   ;;
 
-  21 (t :d4 1 3)
+  21 (p :d4 :vel 100 :decay 1.5)
   ;;
   ;;
   )
 
+;; Repeat part 1
 (part1 (metro))
+(part1 (on-next-bar 3 8))
+
+;; Part 2
+;; TBC
 
 ;;
 ;;(stop)
