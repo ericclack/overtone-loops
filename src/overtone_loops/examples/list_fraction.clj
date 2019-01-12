93bbf1ab781e4649c72857e7814757d47cb951a2
Eric Clack <eric@bn7.net>
2019-01-12 17:25:05 +0000

    Fixed typo

--- src/overtone_loops/examples/list_fraction.clj
+++ src/overtone_loops/examples/list_fraction.clj
@@ -4,33 +4,28 @@
 
 ;; Define some samples from Freesound.org
 (def kick (freesound2 171104))
-(def snare (freesound2 270156))
 (def hat (freesound2 404890))
 (def crash (freesound2 439789))
 (def clap (freesound2 24787))
+(def bell (freesound2 382806))
 
-;; Lists of name, num-beats, instrument and amplitudes, 0=mute
-(defloop hats   (4 1/2) hat    [0    0.5  0    0.5  0    0.5     0    0.5])
-(defloop kicks  (4 1/2) kick   [0.7  0    0.2  0    0.7  0.6     0.2  0  ])
-(defloop claps   4      clap   [0         0         1            0       ])
-(defloop snares (4 1/3) snares [0 0 0     0 0 0     0.5 0.6 0.7  0 0 0   ])
-
-(defloop other-stuff 8
-  0 (crash)
-  5 (crash :rate 0.9)
-  6 (crash)
-  )
+;; Lists of name, beat-pattern, instrument and amplitudes, 0=mute
+;; beat-pattern is either an int = number of beats in a bar
+;; of a pair of (beats-in-bar beat-value)
+(defloop hats   (4 1/2) hat    [0     0.5   0     0.5   0       0.5     0    0.5])
+(defloop kicks  (4 1/2) kick   [0.7   0     0.2   0     0.7     0.6     0.2  0  ])
+(defloop claps   4      clap   [0           0           0.7             0       ])
+(defloop bells  (4 1/3) bell   [0.4 0 0     0 0 0.4     0.5 0.6 0.7     0 0 0   ])
 
 ;; ---------------------------------------------
 
-(bpm 95)
+(bpm 110)
   
 (do
   (hats (on-next-bar 4))
   (kicks (on-next-bar 4))
   (claps (on-next-bar 4))
-  (snares (on-next-bar 4))
-  (other-stuff (on-next-bar 4))
+  (bells (on-next-bar 4))
   )
 
 ;;(stop)
