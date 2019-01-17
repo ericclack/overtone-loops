000789f66520164df0b35a93f3158cad77e50978
Eric Clack <eric@bn7.net>
2019-01-17 15:18:16 +0000

    Added patterns from book Drum Programming Handbook

---
+++ src/overtone_loops/dph_book/pattern_03_01.clj
@@ -0,0 +1,29 @@
+(ns overtone-loops.dph-book.pattern-03-01
+  (:use [overtone.live]
+        [overtone-loops.loops]))
+
+;; Define some samples from Freesound.org
+(def closed-hh (freesound2 404890))
+(def snare (freesound2 270156))
+(def kick (freesound2 171104))
+
+
+(def crash (freesound2 439789))
+(def clap (freesound2 24787))
+
+;;                                         1   &   2   &   3   &   4   & 
+(defloop closed-hhs  (4 1/2) closed-hh    [0.7 0.7 0.7 0.7 0.7 0.7 0.7 0.7 ])
+(defloop sds         (4 1/2) snare        [0   0   0.8 0   0   0   0.8 0   ])
+(defloop kicks       (4 1/2) kick         [0.8 0   0   0   0.8 0   0   0   ])
+  
+;; ---------------------------------------------
+
+(bpm 120)
+  
+(do
+  (closed-hhs (on-next-bar 4))
+  (sds        (on-next-bar 4))
+  (kicks      (on-next-bar 4))
+  )
+
+;;(stop)
