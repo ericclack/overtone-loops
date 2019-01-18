95c3d82913e6abc1f21397b744c1b282671e7542
Eric Clack <eric@bn7.net>
2019-01-18 13:29:01 +0000

    Compact loop syntax

--- src/overtone_loops/examples/list_fraction.clj
+++ src/overtone_loops/examples/list_fraction.clj
@@ -12,10 +12,12 @@
 ;; Lists of name, beat-pattern, instrument and amplitudes, 0=mute
 ;; beat-pattern is either an int = number of beats in a bar
 ;; or a pair of (beats-in-bar beat-value)
-(defloop hats   (4 1/2) hat    [0     0.5   0     0.5   0       0.5     0    0.5  ])
-(defloop kicks  (4 1/2) kick   [0.7   0     0.2   0     0.7     0.6     0.2  0    ])
-(defloop claps   4      clap   [0           0           0.7             0         ])
-(defloop bells  (4 1/3) bell   [0.4 0 0     0 0 0.4     0 0.6 0         0 0.5 0.7 ])
+(defloop hats   (4 1/2) hat    [- 5 - 5 - 5 - 5 ])
+(defloop kicks  (4 1/2) kick   [7 - 2 - 7 6 2 - ])
+(defloop claps   4      clap   [-   -   7   -   ])
+
+;; Thirds
+(defloop bells  (4 1/3) bell   [4 - -  - - 4  - 6 -  - 5 7 ])
 
 ;; ---------------------------------------------
 
