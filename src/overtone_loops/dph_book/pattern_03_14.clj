d1a111147006a2e0b34e8fb7db3dd98f0a2f2c70
Eric Clack <eric@bn7.net>
2019-01-20 20:35:08 +0000

    New patterns

---
+++ src/overtone_loops/dph_book/pattern_03_14.clj
@@ -0,0 +1,32 @@
+(ns overtone-loops.dph-book.pattern-03-14
+  (:use [overtone.live]
+        [overtone-loops.loops]))
+
+;; Define some samples from Freesound.org
+(def closed-hh (freesound2 404890))
+(def open-hh (freesound2 404893))
+(def ride-bell (freesound2 171482))
+(def snare (freesound2 404859))
+(def kick (freesound2 171104))
+
+;; We want to use amps between 0 and 9 in our lists
+(amp-scale 1/9)
+
+;; Quarter beats                       1 e & a 2 e & a 3 e & a 4 e & a  1 e & a 2 e & a 3 e & a 4 e & a 
+(defloop rides      (8 1/4) ride-bell [- - 5 - - - 5 - - - 5 - - - 5 -  - - 5 - - - 5 1 - - 5 - - 1 5 - ])
+
+(defloop sds        (16 1/4) snare    [- - - - 7 - - - - 5 - - 7 - - -  - - - - 7 - - - - - - - 7 - - 5  - 5 - - 7 - 6 - - - - - 7 - - -  - - - - 7 - - - - 5 - - 7 - - - ])
+(defloop kicks      (16 1/4) kick     [6 - - 6 - - - 6 - - 7 - - - - -  6 - - - - - - 5 6 - 7 - - - 7 -  7 - 6 7 - - - 7 - - 7 - - 7 - -  8 - 7 - - 7 - 7 8 - 7 8 - 7 - 6 ])
+;;                                     |       |       |       |        |       |       |       |        |       |       |       |        |       |       |       |
+;; Quarter beats                       1 e & a 2 e & a 3 e & a 4 e & a  1 e & a 2 e & a 3 e & a 4 e & a  1 e & a 2 e & a 3 e & a 4 e & a  1 e & a 2 e & a 3 e & a 4 e & a
+
+(bpm 120)
+(beats-in-bar 4)
+
+(at-bar 1
+        (rides 4) ;; 2 bar phrase
+        (sds 2) ;; 4 bar phrases, twice
+        (kicks 2)
+        )
+
+;;(stop)
