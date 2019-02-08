07170b1f831df868a44c4bd9571f3abab75a19af
Eric Clack <eric@bn7.net>
2019-02-08 17:48:49 +0000

    Make a list of samples

---
+++ src/overtone_loops/instr.clj
@@ -0,0 +1,33 @@
+(ns overtone-loops.instr
+  (:use [overtone-loops.loops]))
+
+;; Define some samples from Freesound.org
+
+(def bell (freesound2 382806)) ;; (bell)
+(def clap (freesound2 24787)) ;; (clap)
+(def crash (freesound2 439789)) ;; (crash)
+
+(def hatopen (freesound2 404893)) ;; (hatopen)
+(def hatc (freesound2 404891))  ;; closed ;; (hatc)
+(def hatc2 (freesound2 404890)) ;; closed ;; (hatc2)
+(def hatc3 (freesound2 96140))  ;; (hat3)
+(def hatp (freesound2 93910))  ;; pedal ;; (hatp)
+
+(def kick (freesound2 171104)) ;; (kick)
+(def kickelec (freesound2 250547)) ;; (kickelec)
+(def kicksoft (freesound2 56430)) ;; (kicksoft)
+
+(def open-hh (freesound2 404893)) ;; (open-hh)
+(def openhat (freesound2 317094)) ;; (openhat)
+(def pedal-hh (freesound2 93910)) ;; (pedal-hh)
+
+(def ride (freesound2 162311)) ;; (ride)
+(def ride-bell (freesound2 171482)) ;; (ride-bell)
+(def rim (freesound2 34831)) ;; (rim)
+
+(def snare (freesound2 151689)) ;; (snare)
+(def snare2 (freesound2 404859)) ;; (snare2)
+
+;; Snaps etc
+(def finger (freesound2 177495)) ;; (finger)
+(def stick (freesound2 82280)) ;; (stick)
