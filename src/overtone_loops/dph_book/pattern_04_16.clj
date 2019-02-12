0fd9b60532f26207e275bab4b9d7226d1ec611fb
Eric Clack <eric@bn7.net>
2019-02-12 09:09:02 +0000

    Local samples

--- src/overtone_loops/dph_book/pattern_04_16.clj
+++ src/overtone_loops/dph_book/pattern_04_16.clj
@@ -1,12 +1,7 @@
 (ns overtone-loops.dph-book.pattern-04-16
   (:use [overtone.live]
-        [overtone-loops.loops]))
-
-;; Define some samples from Freesound.org
-(def closed-hh (freesound2 404890))
-(def open-hh (freesound2 404893))
-(def snare (freesound2 404859))
-(def kick (freesound2 171104))
+        [overtone-loops.loops]
+        [overtone-loops.samples]))
 
 ;; We want to use amps between 0 and 9 in our lists
 (amp-scale 1/9)
@@ -14,14 +9,14 @@
 ;;                    BAR1                              BAR2                                BAR3                                BAR3
 ;;                    1 e & a 2 e & a 3 e & a 4 e & a   1 e & a 2 e & a 3 e & a 4 e & a   1 e & a 2 e & a 3 e & a 4 e & a   1 e & a 2 e & a 3 e & a 4 e & a  
 (defloop open-hhs
-  (16 1/4) open-hh   [- - - - - - - 7 - - - - - - - -   - - 7 - - - - - - - - - - 7 - -   - - - 7 - - - - - - 7 - - - - -   - - 7 - - - - 7 - - - - - - - -  ])
+  (16 1/4) cymbal-open   [- - - - - - - 7 - - - - - - - -   - - 7 - - - - - - - - - - 7 - -   - - - 7 - - - - - - 7 - - - - -   - - 7 - - - - 7 - - - - - - - -  ])
 (defloop closed-hhs
-  (16 1/4) closed-hh [5 3 7 5 5 4 8 - 5 3 6 5 4 4 8 5   4 3 - - 4 5 6 5 4 4 8 6 4 - 8 6   5 3 8 - 5 4 8 4 6 4 - - 4 3 8 6   5 4 - - 5 7 8 - 6 5 8 7 4 4 6 5  ])
+  (16 1/4) cymbal-closed [5 3 7 5 5 4 8 - 5 3 6 5 4 4 8 5   4 3 - - 4 5 6 5 4 4 8 6 4 - 8 6   5 3 8 - 5 4 8 4 6 4 - - 4 3 8 6   5 4 - - 5 7 8 - 6 5 8 7 4 4 6 5  ])
 
 (defloop sds
-  (16 1/4) snare     [- - - - 8 - - - - 6 - - 8 - - -   - - - - 8 - - - - - - - 8 - - 6   - - - - 8 - - - - 6 - - 8 - - -   - - - - 8 - - - - - - - 8 - - 6  ])
+  (16 1/4) snare-hard    [- - - - 8 - - - - 6 - - 8 - - -   - - - - 8 - - - - - - - 8 - - 6   - - - - 8 - - - - 6 - - 8 - - -   - - - - 8 - - - - - - - 8 - - 6  ])
 (defloop kicks
-  (16 1/4) kick      [7 - - 7 - - - 7 - - 8 - - - - -   7 - - - - - - 7 8 - 8 - - - 7 -   7 - - 6 - - - 7 - - 7 - - - - -   7 - - - - - - 6 7 - 7 - - - 7 -  ])
+  (16 1/4) bass-hard     [7 - - 7 - - - 7 - - 8 - - - - -   7 - - - - - - 7 8 - 8 - - - 7 -   7 - - 6 - - - 7 - - 7 - - - - -   7 - - - - - - 6 7 - 7 - - - 7 -  ])
 
 ;; ---------------------------------------------
 
