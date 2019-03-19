542e8e332b4d5425df8f624b8d667be0375c69fc
Eric Clack <eric@bn7.net>
2019-03-19 13:21:15 +0000

    Added emptyloop

--- src/overtone_loops/proto/mar19_2019.clj
+++ src/overtone_loops/proto/mar19_2019.clj
@@ -6,32 +6,43 @@
 ;; Patterns ------------------------------------------------------------
 
 ;;                                      1 e & a 2 e & a 3 e & a 4 e & a
+(defloop ticks  (4 1/2) cymbal-closed  [1   5   1   4   2   5   1   4   ])
+
 (defloop hats   (4 1/4) cymbal-closed  [1 - 5 - 2 3 5 - 8 4 - 1 3 - 2 - ])
 (defloop pedals (4 1/4) cymbal-pedal   [- 3 - 2 - - - - - - 2 - - 7 - 4 ])
 (defloop kicks  (4 1/4) bass-elec      [7 - - - 7 - - 4 2 - - - 7 - - - ])
 
 (defloop clicks  4      finger         [8       -       8       -       ])
-(defloop rides1  4      ride           [4       5       -       3       ]) 
-(defloop rides2  4      ride-bell      [-       -       5       3       ]) 
+(defloop rides1 (4 1/2) ride           [7   7   6   7   8   7   8   6   ]) 
 
-;; ---------------------------------------------
+;; ---------------------------------------------------------------------
 
 (bpm 110) 
 (beats-in-bar 4)
 
 (at-bar 1
+        (ticks 4)
         ;; forever
-        (hats)
-        (pedals)
         (kicks) 
         ) 
 
+(at-bar 5
+        (hats)
+        (pedals)
+        )
+
+(at-bar 13
+        (clicks)
+        )
+
 (comment ; all play for only a few phrases
   ;; Play these with Ctrl-X Ctrl-E
-  (clicks (on-next-bar) 4)
-  
   (rides1 (on-next-bar) 4)
-  (rides2 (on-next-bar) 4)
+
+  ;; To stop any loop, redefine it as empty
+  (emptyloop clicks 4)
+  (emptyloop hats 4)
+  (emptyloop pedals 4)
   )
 
 ;;(stop)
