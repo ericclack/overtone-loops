30024c8c0cbecd62b9a9b70fda37cf8e48fe1620
Eric Clack <eric@bn7.net>
2019-03-26 16:43:48 +0000

    Helper functions

--- src/overtone_loops/music/sticks1.clj
+++ src/overtone_loops/music/sticks1.clj
@@ -1,42 +1,45 @@
 (ns overtone-loops.music.sticks1
   (:use [overtone.live]
         [overtone.inst.synth]
-        [overtone-loops.loops]))
+        [overtone-loops.loops]
+        [overtone-loops.samples]))
 
-;; Define some samples from Freesound.org
-(def kick (freesound2 56430))
-(def stick (freesound2 82280))
+;;                              1 e & a 2 e & a 3 e & a 4 e & a 
+(defloop sticks (4 1/4) stick  [8 - - - 6 - - 6 - - 6 - - 6 - - ]) 
+(defloop kicks  4       kick   [8       8       8       8       ])
+(defloop clicks (4 1/4) finger [- - - 1 - - 2 - - 1 - - - - 3 - ])
 
-;;                             1 e & a 2 e & a 3 e & a 4 e & a 
-(defloop sticks (4 1/4) stick [8 - - - 6 - - 6 - - 6 - - 6 - - ]) 
-(defloop kicks  4       kick  [8       8       8       8       ])
-
+(defn o [n a]
+  (overpad (note n) :amp a))
 
 (defloop bass-line 4
-  0    (overpad (note :f2) :amp 0.4)
-  2    (overpad (note :g2) :amp 0.5)
-  2.5  (overpad (note :b3) :amp 0.3)
-  3.5  (overpad (note :a3) :amp 0.2)
-  3.82 (overpad (note :g2) :amp 0.2)
+  0    (o :f2 0.4)
+  2    (o :g2 0.5)
+  2.5  (o :b3 0.3)
+  3.5  (o :a3 0.2)
+  3.82 (o :g2 0.2)
   )
 
 ;; (emptyloop bass-line 4)
 
+(defn k [n a]
+  (ks1 (note n) :amp a))
+
 (defloop melody1 8
-  0    (ks1 (note :g4) :amp 0.8)
-  1    (ks1 (note :a4) :amp 0.8)
-  2    (ks1 (note :b4) :amp 0.8)
-  3    (ks1 (note :c5) :amp 0.8)
+  0    (k :g4 0.8)
+  1    (k :a4 0.8)
+  2    (k :b4 0.8)
+  3    (k :c5 0.8)
   )
 
 ;; (emptyloop melody1 8)
 
 (defloop melody2 8
-  5.5  (ks1 (note :c5) :amp 0.8)
-  6    (ks1 (note :a4) :amp 0.8)
-  6.5  (ks1 (note :b4) :amp 0.8)
-  7    (ks1 (note :g4) :amp 0.8)   
-  7.5  (ks1 (note :f4) :amp 0.8)   
+  5.5  (k :c5 0.8)
+  6    (k :a4 0.8)
+  6.5  (k :b4 0.8)
+  7    (k :g4 0.8)   
+  7.5  (k :f4 0.8)   
   )
 
 ;; (emptyloop melody2 8)
@@ -49,6 +52,10 @@
 (at-bar 1
         (sticks )
         (kicks )
+        )
+
+(at-bar 3
+        (clicks)
         )
 
 (at-bar 5
