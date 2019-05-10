671da0f04950839c5ac9973e401ec9463443059a
Eric Clack <eric@bn7.net>
2019-05-10 18:51:19 +0100

    More patterns

--- src/overtone_loops/proto/alt_patterns.clj
+++ src/overtone_loops/proto/alt_patterns.clj
@@ -3,13 +3,21 @@
         [overtone-loops.loops]
         [overtone-loops.samples]))
   
-(bpm 105)
+(bpm 125)
 (beats-in-bar 4)
 
-(bars-repeat 4
+(bars-repeat 8
   (play-pattern 1 bass-soft       [4   4   4   4  ])
   (play-pattern 1/2 cymbal-closed [. 4 . 2 . 4 . 2])
-  (play-pattern 1/4 clap          [. . 2 . . 5 1 2])
+  ;;(play-pattern 1/4 clap          [. . 2 . . . . 2])
+  )
+
+
+
+(comment
+  (bars-repeat 2
+               (play-pattern 1/4 clap          [. . 2 . . . . 2])
+               )
   )
 
 
