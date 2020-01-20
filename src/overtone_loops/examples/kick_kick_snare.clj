e5e83744e9e4280dc3b02f5f20d1cd8fb220be40
Eric Clack <eric@bn7.net>
2020-01-20 20:16:57 +0000

    Simpler pattern for comparison with old_style_loop.clj

--- src/overtone_loops/examples/kick_kick_snare.clj
+++ src/overtone_loops/examples/kick_kick_snare.clj
@@ -5,31 +5,17 @@
 
 (set-up)
 
-;;                          1 & 2 & 3 & 4 &  5 & 6 & 7 & 8 &         
-(def hats
-  (loop-player 1/2 hat     [_ 5 _ 5 _ 5 _ 5  5 _ _ 5 5 _ _ 5]))
-
-(def kicks
-  (loop-player 1/2 kick    [8 _ 8 _ _ _ _ _]))
-(def snares
-  (loop-player 1/2 snare   [_ _ _ _ 8 _ _ _  _ _ _ _ 8 _ _ _
-                            _ _ _ _ 7 _ _ _  _ _ _ 3 _ 5 7 _]))
-
-
-
-;; ---------------------------------------------
+;;                           1 & 2 & 3 & 4 &
+(defloop kicks  1/2 kick    [8 _ 7 _ _ _ _ _])
+(defloop snares 1/2 snare   [_ _ _ _ 8 _ _ _])
+(defloop hats   1/2 hat     [_ 5 _ 5 _ 5 _ 5])
 
 (bpm 140)
 (beats-in-bar 4)
-
 (at-bar 1
-  (kicks )
-  (snares )
-  (hats )
-  )
-
-(comment ; all play for only a few phrases
-  ;; Play these with Ctrl-X Ctrl-E
+  (kicks)
+  (snares)
+  (hats)
   )
 
 ;;(stop)
